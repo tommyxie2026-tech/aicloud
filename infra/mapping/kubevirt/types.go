84e98f5ee44d2c5e0a0614aac2482dee6a859a1a
tommyxie2026-tech <tommyxie2026@gmail.com>
2026-06-21 01:06:18 +0900

    Add KubeVirt mapping skeleton types

---
+++ infra/mapping/kubevirt/types.go
@@ -0,0 +1,32 @@
+package kubevirt
+
+type DesiredVirtualMachine struct {
+	Name             string
+	Namespace        string
+	ClusterName      string
+	WorkerGroupName  string
+	Ordinal          int
+	MachineClassName string
+	CPU              string
+	Memory           string
+	GPUProfile       string
+	StorageProfile   string
+	Labels           map[string]string
+}
+
+type MappingResult struct {
+	VirtualMachines []DesiredVirtualMachine
+}
+
+type MappingError struct {
+	Code    string
+	Message string
+}
+
+func NewMappingError(code string, message string) *MappingError {
+	return &MappingError{Code: code, Message: message}
+}
+
+func (e *MappingError) Error() string {
+	return e.Code + ": " + e.Message
+}
