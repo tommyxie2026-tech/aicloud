4b1f8d037b7eb08d9663ed6349145726a5b42ebf
tommyxie2026-tech <tommyxie2026@gmail.com>
2026-08-14 12:35:05 +0900

    S3C use explicit worker signal root context

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"os"
 	"os/signal"
@@ -19,7 +20,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	ctx, stop := signal.NotifyContext(nil, syscall.SIGINT, syscall.SIGTERM)
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
 	if !cfg.Temporal.Enabled {
