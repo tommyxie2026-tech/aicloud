cbc300a1d086d0ab80074a5397b0de14d358ad3e
tommyxie2026-tech <tommyxie2026@gmail.com>
2026-08-14 12:34:50 +0900

    S3C replace worker skeleton with bounded Temporal lifecycle

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -5,12 +5,45 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
+
+	"github.com/tommyxie2026-tech/aicloud/internal/config"
+	"github.com/tommyxie2026-tech/aicloud/internal/workflow"
 )
 
 func main() {
+	cfg := config.Load()
 	log := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-	log.Info("aicloud worker started", "mode", "skeleton", "message", "workflow and agent workers are reserved for the next sprint")
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
-	<-signals
+	if err := cfg.Temporal.Validate(); err != nil {
+		log.Error("Temporal worker configuration invalid", "error", err)
+		os.Exit(1)
+	}
+
+	ctx, stop := signal.NotifyContext(nil, syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	if !cfg.Temporal.Enabled {
+		log.Info("aicloud worker idle", "temporalEnabled", false)
+		<-ctx.Done()
+		return
+	}
+
+	workerConfig := workflow.WorkerConfig{
+		Enabled:     true,
+		Address:     cfg.Temporal.Address,
+		Namespace:   cfg.Temporal.Namespace,
+		TaskQueue:   cfg.Temporal.TaskQueue,
+		StopTimeout: time.Duration(cfg.Temporal.WorkerStopTimeoutSeconds) * time.Second,
+	}
+	log.Info(
+		"aicloud Temporal worker starting",
+		"address", workerConfig.Address,
+		"namespace", workerConfig.Namespace,
+		"taskQueue", workerConfig.TaskQueue,
+	)
+	if err := workflow.RunTemporalWorker(ctx, workerConfig, workflow.FailClosedLifecycleActivities{}); err != nil {
+		log.Error("aicloud Temporal worker stopped with error", "error", err)
+		os.Exit(1)
+	}
+	log.Info("aicloud Temporal worker stopped")
 }
